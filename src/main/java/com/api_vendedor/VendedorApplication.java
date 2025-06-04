ef1864e278520ed61186501853fd7a5d5ce2924b
Skibidi-Mate <mate.hernandezm@duocuc.cl>
2025-06-04 18:03:56 -0400

    se creo model vendedor

--- src/main/java/com/api_vendedor/VendedorApplication.java
+++ src/main/java/com/api_vendedor/VendedorApplication.java
@@ -1,4 +1,4 @@
-package com.api_vendedor.vendedor;
+package com.api_vendedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/api_vendedor/models/Vendedor.java
+++ src/main/java/com/api_vendedor/models/Vendedor.java
@@ -1,5 +1,25 @@
 package com.api_vendedor.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "vendedores")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vendedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idVendedor;
+
+    private Integer idUsuario;
+
+    private String nombreCompleto;
+
+    private String rut;
+
+    private String areaVentas;
 
 }
